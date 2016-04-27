fe76575ff4d8db9c778acc81ba3d50de12f98b75
codingswitf <wutthiphorn@gmail.com>
2016-04-27 23:16:48 +0700

    27/04/2559
    json

---
+++ test.php
@@ -0,0 +1,22 @@
+<?php
+$servername = "sql6.freesqldatabase.com";
+$username = "sql6117117";
+$password = "jBdzlc1xTi";
+$dbname = "sql6117117";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$sql = "SELECT * FROM `register` LIMIT 0, 30 ";
+$conn->query("SET names utf8");
+$myArray = array();
+$result = $conn->query($sql);
+  while($row = $result->fetch_array(MYSQL_ASSOC)){
+      $myArray[]=$row;
+  }
+
+echo json_encode($myArray,JSON_UNESCAPED_UNICODE);
+$conn->close();
+?>	
