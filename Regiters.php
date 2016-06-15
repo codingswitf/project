1120adb730b886d5254f811aff93380684a1fd96
codingswitf <wutthiphorn.coding@gmail.com>
2016-06-15 23:58:21 +0700

    frank
    frank

--- Regiters.php
+++ Regiters.php
@@ -9,17 +9,17 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$dataID = $_POST["id"];
+//$dataID = $_POST["id"];
 //$dataID = "31";
 $str = "Hello";
 echo md5($str);
-$sql = "SELECT detail FROM `datatable` WHERE dataID = '".$dataID."' ";
-$conn->query("SET names utf8");
-$myArray = array();
-$result = $conn->query($sql);
-  while($row = $result->fetch_array(MYSQL_ASSOC)){
-      $myArray[]=$row;
-  }
-echo json_encode($myArray,JSON_UNESCAPED_UNICODE);
-$conn->close();
+//$sql = "SELECT detail FROM `datatable` WHERE dataID = '".$dataID."' ";
+//$conn->query("SET names utf8");
+//$myArray = array();
+//$result = $conn->query($sql);
+ // while($row = $result->fetch_array(MYSQL_ASSOC)){
+ //     $myArray[]=$row;
+//  }
+//echo json_encode($myArray,JSON_UNESCAPED_UNICODE);
+//$conn->close();
 ?>
